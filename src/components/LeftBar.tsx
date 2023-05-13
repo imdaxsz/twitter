57f315839cf76a56d68a1b259ba29c370caa5e67
mihee <s2809h29@gmail.com>
2023-05-13 16:32:39 +0900

    feat: 로그아웃 기능 추가

--- src/components/LeftBar.tsx
+++ src/components/LeftBar.tsx
@@ -1,39 +1,63 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { Link, useLocation } from "react-router-dom";
+import { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { User } from "firebase/auth";
 import { RiMoreFill, RiNotification2Line } from "react-icons/ri";
-import { FaHashtag } from "react-icons/fa";
+import { FaHashtag, FaTwitter } from "react-icons/fa";
 import { AiFillHome } from "react-icons/ai";
 import { HiOutlineBookmark } from "react-icons/hi";
-
 import { BiUser } from "react-icons/bi";
+import { auth } from "fBase";
+import styles from "styles/leftbar.module.css";
 
 function LeftBar({ userObj }: { userObj: User | null }) {
   const location = useLocation();
-  
+  const [modal, setModal] = useState(false);
+  const navigate = useNavigate();
+
+  const onProfileClick = () => {
+    setModal(true);
+  };
+
+  const onProfileBlur = () => {
+    setTimeout(() => setModal(false), 100);
+  };
+
+  const onLogOutClick = () => {
+    const ok = window.confirm("로그아웃할까요?");
+    if (ok) {
+      auth.signOut();
+      navigate("/");
+    }
+  };
+
   return (
-    <div className="leftbar-container">
+    <div className={styles.container}>
+      {modal && (
+        <div onClick={onLogOutClick} className={`${styles.modal} modal-shadow flex`}>
+          <h4>로그아웃</h4>
+        </div>
+      )}
       <div>
         <div className="logo-box">
           <Link to="/">
-            <FontAwesomeIcon icon={faTwitter} className="logo" size="2xl" />
+            <FaTwitter className={`logo ${styles.pl}`} />
+            {/* <FontAwesomeIcon icon={faTwitter} className={styles.logo} size="2xl" /> */}
           </Link>
         </div>
-        <nav className="navbar">
+        <nav className={styles.navbar}>
           <ul>
             <li>
               <Link to="/">
-                <div className={`nav-item ${location.pathname === "/" ? "active" : ""}`}>
-                  <AiFillHome className="nav-icon" />
+                <div className={`${styles["nav-item"]} ${location.pathname === "/" ? styles.active : ""}`}>
+                  <AiFillHome className={styles["nav-icon"]} />
                   <span>홈</span>
                 </div>
               </Link>
             </li>
             <li>
               <Link to="/explore">
-                <div className={`nav-item ${location.pathname === "/explore" ? "active" : ""}`}>
-                  <FaHashtag className="nav-icon" />
+                <div className={`${styles["nav-item"]} ${location.pathname === "/explore" ? styles.active : ""}`}>
+                  <FaHashtag className={styles["nav-icon"]} />
                   <span>
                     <b>탐색하기</b>
                   </span>
@@ -42,24 +66,24 @@ function LeftBar({ userObj }: { userObj: User | null }) {
             </li>
             <li>
               <Link to="/notifications">
-                <div className={`nav-item ${location.pathname === "/notifications" ? "active" : ""}`}>
-                  <RiNotification2Line className="nav-icon" />
+                <div className={`${styles["nav-item"]} ${location.pathname === "/notifications" ? styles.active : ""}`}>
+                  <RiNotification2Line className={styles["nav-icon"]} />
                   <span>알림</span>
                 </div>
               </Link>
             </li>
             <li>
               <Link to="/bookmarks">
-                <div className={`nav-item ${location.pathname === "/bookmarks" ? "active" : ""}`}>
-                  <HiOutlineBookmark className="nav-icon" />
+                <div className={`${styles["nav-item"]} ${location.pathname === "/bookmarks" ? styles.active : ""}`}>
+                  <HiOutlineBookmark className={styles["nav-icon"]} />
                   <span>북마크</span>
                 </div>
               </Link>
             </li>
             <li>
               <Link to="/profile">
-                <div className={`nav-item ${location.pathname === "/profile" ? "active" : ""}`}>
-                  <BiUser className="nav-icon" />
+                <div className={`${styles["nav-item"]} ${location.pathname === "/profile" ? styles.active : ""}`}>
+                  <BiUser className={styles["nav-icon"]} />
                   <span>프로필</span>
                 </div>
               </Link>
@@ -68,24 +92,22 @@ function LeftBar({ userObj }: { userObj: User | null }) {
         </nav>
         <button className="btn medium">트윗하기</button>
       </div>
-      <Link to="/profile">
-        <div className="profile-box">
-          <div className="profile-img ">
-            <img src={userObj?.photoURL ? userObj.photoURL : "img/default_profile.png"} alt="userImg"></img>
+      <div className={styles.profile} tabIndex={-1} onClick={onProfileClick} onBlur={onProfileBlur}>
+        <div className={styles["profile-img"]}>
+          <img src={userObj?.photoURL ? userObj.photoURL : "img/default_profile.png"} alt="userImg"></img>
+        </div>
+        <div className={`${styles["profile-info"]} flex`}>
+          <div className={`${styles["profile-item"]} flex`}>
+            <h4>{userObj?.displayName}</h4>
           </div>
-          <div className="profile-info flex">
-            <div className="profile-item flex">
-              <h3>{userObj?.displayName}</h3>
-            </div>
-            <div className="profile-item flex">
-              <p>@{userObj?.email?.split("@")[0]}</p>
-            </div>
-          </div>
-          <div className="more-box right">
-            <RiMoreFill className="icon" />
+          <div className={`${styles["profile-item"]} flex`}>
+            <p>@{userObj?.email?.split("@")[0]}</p>
           </div>
         </div>
-      </Link>
+        <div className="twticon-box more right">
+          <RiMoreFill className="icon" />
+        </div>
+      </div>
     </div>
   );
 }
