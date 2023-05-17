e7073036ace89cf13f386a6afe1eeab72fb61ab1
mihee <s2809h29@gmail.com>
2023-05-17 20:01:02 +0900

    fix: 검색창 focus 개선
    기존에 input 태그 영역을 클릭할 때에만 input에 focus가 되던 것을 검색창 모든 영역 클릭 시 focus 되도록 수정

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,7 @@
 import { useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleXmark, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import styles from "styles/searchbar.module.css";
 
 function SearchBar() {
   const [word, setWord] = useState("");
@@ -15,25 +16,25 @@ function SearchBar() {
     console.log(word);
   };
 
+  const focusSearchBar = () => {
+    if (focusRef.current instanceof HTMLInputElement) focusRef.current.focus();
+  };
+
   const clearWord = (e: React.MouseEvent<HTMLSpanElement>) => {
-    if (focusRef.current instanceof HTMLInputElement)
-      focusRef.current.focus();
+    focusSearchBar();
     setWord("");
   };
 
   return (
     <form onSubmit={onSubmit} aria-label="트위터 검색" role="search">
-      <div className="search__container">
-        <div className="searchbar__icon">
+      <div className={styles.container} onClick={focusSearchBar}>
+        <div className={styles.icon}>
           <FontAwesomeIcon icon={faMagnifyingGlass} />
         </div>
-        {/* <div className="searchbar__input">
-          <input onChange={onChange} type="text" value={word} placeholder="트위터 검색"></input>
-        </div> */}
-        <input onChange={onChange} type="text" value={word} placeholder="트위터 검색" className="searchbar__input" ref={focusRef}></input>
+        <input onChange={onChange} type="text" value={word} placeholder="트위터 검색" className={styles.input} ref={focusRef}></input>
 
         {word !== "" && (
-          <span className="clear" onClick={clearWord}>
+          <span tabIndex={0} className={styles.clear} onClick={clearWord}>
             <FontAwesomeIcon icon={faCircleXmark} size="lg" />
           </span>
         )}
