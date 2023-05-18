c4b5775bc3f8422a0988b26035c357fc20acdcb9
mihee <s2809h29@gmail.com>
2023-05-18 18:55:03 +0900

    design: 트윗에 이미지 포함 시 이미지 배치를 포함한 UI 수정
    TweetFactory.tsx
    이미지를 포함하여 트윗 작성 시 이미지 미리보기 크기 및 위치 변경
    이미지 삭제 버튼 디자인 및 위치 변경
    텍스트 입력 영역을 textarea로 수정하고 글자수에 따라 자동으로 트윗폼 높이도 늘어나게 함
    
    Tweet.tsx
    트윗 내 이미지 크기 및 위치 변경
    
    imageCompress.ts
    이미지 압축 기능 구현

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,32 +1,59 @@
-import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { deleteDoc, doc, onSnapshot } from "firebase/firestore";
 import { dbService, storageService } from "fBase";
 import { deleteObject, ref } from "firebase/storage";
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash, faPencilAlt } from "@fortawesome/free-solid-svg-icons";
+import React, { useState, useEffect } from "react";
+import { RiMoreFill } from "react-icons/ri";
+import { TbMessageCircle2 } from "react-icons/tb";
+import { RiHeart3Line } from "react-icons/ri";
+import { FiBookmark } from "react-icons/fi";
+import { AiOutlineRetweet, AiFillEdit } from "react-icons/ai";
+import { CgTrash } from "react-icons/cg";
+import styles from "styles/tweet.module.css";
 
 export interface TweetType {
   id: string;
   text: string;
   createdAt: number;
   creatorId: string | undefined;
+  creatorUid: string;
   attachmentUrl: string;
+  likes: number;
+  retweets: number;
+  replies: string[];
 }
 
 interface TweetProps {
   tweetObj: TweetType;
   isOwner: boolean;
+  // userImg: string;
 }
 
-const Tweet = ({ tweetObj, isOwner }: TweetProps) => {
+function Tweet({ tweetObj, isOwner }: TweetProps) {
   const tweetTextRef = doc(dbService, "tweets", `${tweetObj.id}`);
   const urlRef = ref(storageService, tweetObj.attachmentUrl);
 
-  const [editing, setEditing] = useState(false);
-  const [newTweet, setNewTweet] = useState(tweetObj.text);
+  // const [editing, setEditing] = useState(false);
+  // const [newTweet, setNewTweet] = useState(tweetObj.text);
+
+  const [more, setMore] = useState(false);
+  const [userImg, setUserImg] = useState("");
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    getuserImg(tweetObj.creatorUid);
+  }, []);
+
+  const getuserImg = (uid: string) => {
+    onSnapshot(doc(dbService, "profiles", uid), (doc) => {
+      if (doc.exists()) {
+        setUserImg(doc.data().profileImg);
+        setName(doc.data().name);
+      }
+    });
+  };
 
   const onDeleteClick = async () => {
-    const ok = window.confirm("이 트윗을 삭제하시겠습니까?");
+    const ok = window.confirm("트윗을 삭제할까요?");
     if (ok) {
       await deleteDoc(tweetTextRef);
       if (tweetObj.attachmentUrl !== "") {
@@ -35,19 +62,103 @@ const Tweet = ({ tweetObj, isOwner }: TweetProps) => {
     }
   };
 
-  const toggleEditing = () => setEditing((prev) => !prev);
-
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    await updateDoc(tweetTextRef, { text: newTweet });
-    setEditing(false);
+  const onMoreClick = () => {
+    setMore(true);
   };
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => setNewTweet(e.target.value);
+  const onMoreBlur = () => {
+    setTimeout(() => setMore(false), 100);
+  };
+
+  // const toggleEditing = () => setEditing((prev) => !prev);
+
+  // const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   await updateDoc(tweetTextRef, { text: newTweet });
+  //   setEditing(false);
+  // };
+
+  // const onChange = (e: React.ChangeEvent<HTMLInputElement>) => setNewTweet(e.target.value);
 
   return (
-    <div className="tweet">
-      {editing ? (
+    <div className={styles.tweet}>
+      {more && (
+        <div className="more-modal modal-shadow">
+          <div className="more-item-box">
+            <div className="more-item">
+              <AiFillEdit className="icon" />
+              <h4>수정하기</h4>
+            </div>
+          </div>
+          <div className="more-item-box" onClick={onDeleteClick}>
+            <div className="more-item red">
+              <CgTrash className="icon red" />
+              <h4>삭제하기</h4>
+            </div>
+          </div>
+        </div>
+      )}
+      <div className={styles["user-img"]}>
+        <img src={userImg !== "" ? userImg : "img/default_profile.png"} alt="userImg"></img>
+      </div>
+      <div className={styles.content}>
+        <div className="flex mb-2">
+          <div className={`${styles.info} flex`}>
+            <div className={styles["user-name"]}>
+              {/* <h4>{tweetObj.creatorName}</h4> */}
+              <h4>{name}</h4>
+            </div>
+            <div className={styles["user-id"]}>
+              <p>{tweetObj.creatorId}</p>
+            </div>
+          </div>
+          {isOwner && (
+            <button title="더 보기" className="twticon-box more blue" onFocus={onMoreClick} onBlur={onMoreBlur}>
+              <RiMoreFill className="icon" />
+            </button>
+          )}
+        </div>
+        {tweetObj.text !== "" && (
+          <div className={styles.text}>
+            <h4>{tweetObj.text}</h4>
+          </div>
+        )}
+        {tweetObj.attachmentUrl !== "" && (
+          <div className={styles.twtimg}>
+            <img src={tweetObj.attachmentUrl} alt={tweetObj.attachmentUrl} />
+          </div>
+        )}
+        {/* <div style={{ clear: "both" }}></div> */}
+        <div className={styles.icons}>
+          <div title="답글" className={`twticon-box blue ${styles.twt} ${styles["l-1"]}`}>
+            <TbMessageCircle2 className="icon" />
+          </div>
+          <div title="리트윗" className={`twticon-box green ${styles.twt} ${styles["l-2"]}`}>
+            <AiOutlineRetweet className="icon" />
+          </div>
+          <div title="마음에 들어요" className={`twticon-box pink ${styles.twt} ${styles["l-3"]}`}>
+            <RiHeart3Line className="icon" />
+          </div>
+          <div title="북마크" className={`twticon-box blue ${styles.twt} ${styles["l-4"]}`}>
+            <FiBookmark className="icon" />
+          </div>
+        </div>
+      </div>
+      {/* <>
+        <h4>{tweetObj.text}</h4>
+        {tweetObj.attachmentUrl && <img src={tweetObj.attachmentUrl} alt={tweetObj.attachmentUrl} />}
+        {isOwner && (
+          <div className="tweet__actions">
+            <span onClick={onDeleteClick}>
+              <FontAwesomeIcon icon={faTrash} />
+            </span> */}
+      {/* <span onClick={toggleEditing}>
+              <FontAwesomeIcon icon={faPencilAlt} />
+            </span> */}
+      {/* </div>
+        )}
+      </> */}
+      {/* {editing ? (
         <>
           <form onSubmit={onSubmit} className="container tweetEdit">
             <input type="text" placeholder="Edit your tweet" value={newTweet} required autoFocus onChange={onChange} className="formInput" />
@@ -57,24 +168,9 @@ const Tweet = ({ tweetObj, isOwner }: TweetProps) => {
             Cancel
           </span>
         </>
-      ) : (
-        <>
-          <h4>{tweetObj.text}</h4>
-          {tweetObj.attachmentUrl && <img src={tweetObj.attachmentUrl} alt={tweetObj.attachmentUrl} />}
-          {isOwner && (
-            <div className="tweet__actions">
-              <span onClick={onDeleteClick}>
-                <FontAwesomeIcon icon={faTrash} />
-              </span>
-              <span onClick={toggleEditing}>
-                <FontAwesomeIcon icon={faPencilAlt} />
-              </span>
-            </div>
-          )}
-        </>
-      )}
+      ) : ( */}
     </div>
   );
-};
+}
 
 export default Tweet;
--- src/components/TweetFactory.tsx
+++ src/components/TweetFactory.tsx
@@ -1,31 +1,43 @@
 import { v4 as uuidv4 } from "uuid";
 import { dbAddDoc, dbCollection, dbService, storageService } from "fBase";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { getDownloadURL, ref, uploadString } from "firebase/storage";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
-import { IoImageOutline } from "react-icons/io5";
-import { VscSmiley } from "react-icons/vsc";
+import { IoImageOutline, IoCloseCircleSharp } from "react-icons/io5";
+import { TfiClose } from "react-icons/tfi";
+import { VscSmiley, VscChromeClose } from "react-icons/vsc";
 // import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { useSelector } from "react-redux";
-import { RootState } from 'store/store';
+import { RootState } from "store/store";
+import useImageCompress from "hooks/imageCompress";
 
 /* Create Tweet Component*/
-const TweetFactory = ({uid}:{uid:string}) => {
+const TweetFactory = ({ uid }: { uid: string }) => {
   const [tweet, setTweet] = useState("");
-  const [attachment, setAttachment] = useState("");  
+  const [attachment, setAttachment] = useState("");
   const user = useSelector((state: RootState) => state.user);
+  const { isLoading: isCompressLoading, compressImage } = useImageCompress();
+
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+
+  useEffect(() => {
+    if (textareaRef && textareaRef.current) {
+      textareaRef.current.style.height = "0px";
+      const scrollHeight = textareaRef.current.scrollHeight;
+      textareaRef.current.style.height = scrollHeight + "px";
+    }
+  }, [tweet]);
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    if (tweet === "") return;
     e.preventDefault();
     let attachmentUrl = "";
 
     if (attachment !== "") {
       // 파일 경로 참조 만들기
       const attachmentRef = ref(storageService, `${uid}/${uuidv4()}`);
+
       // storage 참조 경로로 파일 업로드 하기
       const response = await uploadString(attachmentRef, attachment, "data_url");
+
       // storage 참조 경로에 있는 파일의 URL을 다운로드해서 attchmentUrl에 넣어서 업데이트
       attachmentUrl = await getDownloadURL(response.ref);
       // reponse.ref == attachmentref
@@ -36,11 +48,10 @@ const TweetFactory = ({uid}:{uid:string}) => {
       createdAt: Date.now(),
       creatorId: user.id,
       creatorUid: uid,
-      creatorName: user.name,
       attachmentUrl,
       likes: 0,
       retweets: 0,
-      replies:[]
+      replies: [],
     };
 
     try {
@@ -65,20 +76,24 @@ const TweetFactory = ({uid}:{uid:string}) => {
     }
   };
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setTweet(e.currentTarget.value);
   };
 
   const fileInput = useRef<HTMLInputElement>(null);
 
-  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const {
       currentTarget: { files },
     } = e;
     if (files instanceof FileList) {
       const theFile = files[0];
+      const compressedImage = await compressImage(theFile);
+      if (!compressedImage) return;
+      // const imageUrl = URL.createObjectURL(compressedImage);
+      // setAttachment(imageUrl);
       const reader = new FileReader();
-      reader.readAsDataURL(theFile); //data_url
+      reader.readAsDataURL(compressedImage); //data_url
       reader.onloadend = (finishedEvent) => {
         console.log(finishedEvent);
         if (finishedEvent.target && typeof finishedEvent.target.result == "string") {
@@ -97,17 +112,32 @@ const TweetFactory = ({uid}:{uid:string}) => {
 
   return (
     <form onSubmit={onSubmit}>
+      {/* <Loading loading={true } /> */}
       <div className="factoryInput__container">
         <div className="user-img">
-          <img src={user.profileImg !=="" ? user.profileImg : "img/default_profile.png"} alt="userimg"></img>
+          <img referrerPolicy="no-referrer" src={user.profileImg !== "" ? user.profileImg : "img/default_profile.png"} alt="userimg"></img>
         </div>
-        <div>
+        <div className="factory__content">
           <div className="input-box">
-            <input className="factoryInput__input" value={tweet} onChange={onChange} type="text" placeholder="무슨 일이 일어나고 있나요?" maxLength={120} />
+            <textarea className="factoryInput__input" ref={textareaRef} value={tweet} onChange={onChange} placeholder="무슨 일이 일어나고 있나요?" maxLength={150} />
           </div>
+          {attachment && (
+            <div className="factoryForm__attachment">
+              <img
+                src={attachment}
+                style={{
+                  backgroundImage: attachment,
+                }}
+                alt={attachment}
+              />
+              <div className="factoryForm__clear" onClick={onClearAttachment}>
+                <VscChromeClose className="clear-icon" />
+              </div>
+            </div>
+          )}
           <div className="input-box2">
-            <input type="submit" value="트윗하기" disabled={tweet===""} className="btn small tweet-btn" />
-            <div className="icon-box">
+            <input type="submit" value="트윗하기" disabled={tweet === "" && attachment === ""} className="btn small tweet-btn" />
+            <div title="미디어" className="icon-box">
               <label htmlFor="attach-file">
                 <IoImageOutline className="tweet-form-icon" />
               </label>
@@ -117,21 +147,7 @@ const TweetFactory = ({uid}:{uid:string}) => {
           </div>
         </div>
       </div>
-      {attachment && (
-        <div className="factoryForm__attachment">
-          <img
-            src={attachment}
-            style={{
-              backgroundImage: attachment,
-            }}
-            alt={attachment}
-          />
-          <div className="factoryForm__clear" onClick={onClearAttachment}>
-            <span>Remove</span>
-            <FontAwesomeIcon icon={faTimes} />
-          </div>
-        </div>
-      )}
+      {/* {isCompressLoading && "이미지 압축 중.."} */}
     </form>
   );
 };
