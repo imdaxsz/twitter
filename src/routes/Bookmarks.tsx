9d4b3784dbac0940fe81b0ae2848e5b1343d23e7
mihee <s2809h29@gmail.com>
2023-05-25 23:54:34 +0900

    fix: 북마크에 추가했던 트윗이 삭제 되었을 때 북마크 페이지에서도 삭제되도록 함

--- src/routes/Bookmarks.tsx
+++ src/routes/Bookmarks.tsx
@@ -2,20 +2,38 @@ import TopBar from "components/TopBar";
 import Tweet from "components/Tweet";
 import { useSelector } from "react-redux";
 import { RootState } from "store/store";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import Loading from "components/Loading";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { dbService } from "fBase";
 
 function BookMarks({ uid }: { uid: string }) {
   const [loading, setLoading] = useState(true);
   const tweets = useSelector((state: RootState) => state.user.bookmarks);
 
+  useEffect(() => {
+    tweets.forEach(async (a) => {
+      const docRef = doc(dbService, "tweets", a.id);
+      const docSnap = await getDoc(docRef);
+      if (!docSnap.exists()) {
+        const userRef = doc(dbService, "users", uid);
+        const result = tweets.filter((tweet) => tweet.id !== a.id);
+        await updateDoc(userRef, { bookmarks: [...result] });
+      }
+    })
+    setLoading(false);
+    return () => {
+      setLoading(true);
+    }
+  }, []);
+
   return (
     <div className="wrapper">
-      {/* <Loading loading={loading } /> */}
+      <Loading loading={loading } />
       <TopBar title={"북마크"} uid={uid} />
       <div className="container">
         <div>
-          {tweets.map((tweet, i) => {
+          {tweets.map((tweet) => {
             return <Tweet key={tweet.id} tweetObj={tweet} uid={uid} />;
           })}
         </div>
