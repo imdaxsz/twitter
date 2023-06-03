e6eb2ae640d895f331d97cf7634b6d376561fd61
mihee <s2809h29@gmail.com>
2023-06-03 19:27:44 +0900

    design: 알림 컴포넌트 UI 디자인 및 트윗, 상단바 디자인 일부 수정

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -9,3 +9,14 @@ export const dataURItoFile = (dataURI: string) => {
   const blob = new Blob([ab], { type: mimeString });
   return new File([blob], "uploadImage", { type: mimeString });
 };
+
+export const getTweetDate = (createdAt: number) => {
+  const dt = new Date(createdAt);
+  const year = dt.getFullYear();
+  const month = dt.getMonth() + 1;
+  const date = dt.getDate();
+  const hours = dt.getHours() - 12;
+  const ampm = dt.getHours() > 12 ? "오후" : "오전";
+  const minutes = ("0" + dt.getMinutes()).slice(-2);
+  return { year, month, date, hours, ampm, minutes };
+};
